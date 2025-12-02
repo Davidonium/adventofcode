f5464242dddb45380878a03d82c005dedf6ef764
dhernando <david.hernando91@gmail.com>
2025-12-02 06:30:33 +0100

    mkday with a test

--- 2025/mkday.sh
+++ 2025/mkday.sh
@@ -26,7 +26,6 @@ import (
 
 func main() {
 	fd, err := os.Open("./input.txt")
-
 	if err != nil {
 		fmt.Printf("failed to read input file: %v\n", err)
 		os.Exit(1)
@@ -34,11 +33,74 @@ func main() {
 
 	defer fd.Close()
 
-	scanner := bufio.NewScanner(fd)
+	if err := run(fd); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to run the program: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run(in io.Reader) error {
+    scanner := bufio.NewScanner(in)
 
 	for scanner.Scan() {
 		t := scanner.Text()
         // TODO start coding!
 	}
+
+    return nil
 }
+END
+
+tee -a $DAY_DIR/main_test.go << END
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestChallenge(t *testing.T) {
+	tests := []struct {
+		name        string
+		input       string
+	}{
+        {
+            // TODO test case
+        },
+    }
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			stdout := os.Stdout
+
+			r, w, err := os.Pipe()
+			if err != nil {
+				t.Fatalf("failed to create os.Pipe: %v", err)
+			}
+
+			os.Stdout = w
+
+			in := strings.NewReader(strings.TrimSpace(tt.input))
+
+			if err := run(in); err != nil {
+				w.Close()
+				os.Stdout = stdout
+				t.Fatalf("failed to run the program")
+			}
+
+			w.Close()
+			os.Stdout = stdout
+
+			output, err := io.ReadAll(r)
+			if err != nil {
+				t.Fatalf("could not read from output: %v", err)
+			}
+
+            // TODO test the thing
+		})
+	}
+}
+
 END
